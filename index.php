1025ed41295773e7bde7897f42d2e28eaa874800
Jean-Luc FRITZ <jlfritz@diatem.net>
2015-11-19 14:47:30 +0100

    Mise à jour Joomla 3

--- index.php
+++ index.php
@@ -1,68 +1,64 @@
 <?php
 defined('_JEXEC') or die;
-$index = false;
 
-$lang = JFactory::getLanguage();
-$app = JFactory::getApplication();
-$menu = $app->getMenu();
+$app             = JFactory::getApplication();
+$doc             = JFactory::getDocument();
+$user            = JFactory::getUser();
+$this->language  = $doc->language;
+$this->direction = $doc->direction;
 
-if ($menu->getActive() != '' && $menu->getActive() == $menu->getDefault($lang->getTag())) {
-    $index = true;
-}
+$doc->setGenerator('');
+
+
+// Getting params from template
+$params = $app->getTemplate(true)->params;
+$option   = $app->input->getCmd('option', '');
+$view     = $app->input->getCmd('view', '');
+$layout   = $app->input->getCmd('layout', '');
+$task     = $app->input->getCmd('task', '');
+$itemid   = $app->input->getCmd('Itemid', '');
+$sitename = $app->get('sitename');
 
 ?>
 <!doctype html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction ?>">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
     <head>
-        <meta charset="utf-8" />
-        <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/js/jquery/jquery-1.10.2.min.js"></script>
-        <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/js/jquery/jquery-ui.min.js"></script>
-        
-        <jdoc:include type="head" />
-
-        <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" />
-        <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/style.css" />
-        <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" />
-
-
-        <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/js/jquery/jquery-ui.min.css"  />
-        <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/dStyle_v1.css" />
-        <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/global.css" />        
-        <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/responsive.css" />
-
+        <meta charset="utf-8" />                    
+		<meta name="viewport" content="width=device-width" />
+		
+        <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" />       
+        <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" />              
+        <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/app.css" />        
+       
+		<script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/js/jquery/jquery-1.10.2.min.js"></script>  
         <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/js/cookiesplease.min.js"></script>
-        
+        		
         <link rel="shortcut icon" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/favicon.ico">
-
-        <meta name="viewport" content="width=device-width,initial-scale=0.7" />	
+		
+		<jdoc:include type="head" />
+        	
 </head>
-<body class="<?php echo $this->language; ?>">
-    <div id="fb-root"></div>
-    <script>(function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {
-                return;
-            }
-            js = d.createElement(s);
-            js.id = id;
-            js.src = "//connect.facebook.net/fr_FR/all.js#xfbml=1";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));</script>
-
+<body class="site <?php echo $option
+	. ' view-' . $view
+	. ($layout ? ' layout-' . $layout : ' no-layout')
+	. ($task ? ' task-' . $task : ' no-task')
+	. ($itemid ? ' itemid-' . $itemid : '')
+	. ($params->get('fluidContainer') ? ' fluid' : '');
+	echo ($this->direction == 'rtl' ? ' rtl' : '');
+?>">
     
     
-    <div id="logo" ><a href="/"><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/images/LOGO" border="0" /></a></div>    
     
-    
-    <jdoc:include type="modules" name="header" />
-    <jdoc:include type="modules" name="nav" />        
-    <jdoc:include type="modules" name="beforeComponent" />       
+    <jdoc:include type="modules" name="header" style="none" />
+    <jdoc:include type="modules" name="nav" style="none" />        
+    <jdoc:include type="modules" name="beforeComponent" style="none" />       
     <jdoc:include type="message" />        
     <jdoc:include type="component" /> 	
-    <jdoc:include type="modules" name="inComponent" /> 
-    <jdoc:include type="modules" name="afterComponent" />
-    <jdoc:include type="modules" name="footer" />   
+    <jdoc:include type="modules" name="inComponent" style="none" /> 
+    <jdoc:include type="modules" name="afterComponent" style="none" />
+    <jdoc:include type="modules" name="footer" style="none" />   
     
     <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/js/global.js"></script>
+	<jdoc:include type="modules" name="debug" style="none" />
 </body>
 </html>
